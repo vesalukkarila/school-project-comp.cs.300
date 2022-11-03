a90e4213039a4ad2ee113ec0ba08f0a04be667ee
Vesa Lukkarila <vesalukkarila@eduroam-234-200.uta.fi>
2022-11-03 12:46:01 +0200

    pushback

--- wk09/improving_functions/improve1.cc
+++ wk09/improving_functions/improve1.cc
@@ -12,16 +12,11 @@ using namespace std;
 std::vector<int> ascendingVector(int n){
     std::vector<int> v;
 
-    for ( int i = 0; i<n; ++i)
-        v.push_back(i);
+    // push_back() amortized constant O(n)
+    for ( int i = 0; i<n; ++i)      //O(n)
+        v.push_back(i);             // a.constant
 
 
-    /* O(n2)
-    for ( int i = 0; i < n ; i++){  //O(n)
-        v.insert(v.end(), i);       // O(n)
-    }
-    */
-    for (auto n : v)
-        cout << n << endl;
+
     return v;
 }
