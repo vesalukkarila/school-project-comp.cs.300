a4cc647c32de95d0103fd6e15c40230774ce32d2
Vesa Lukkarila <vesalukkarila@MacBook-Pro-5.local>
2022-11-04 09:38:12 +0200

    iota

--- wk09/improving_functions/improve1.cc
+++ wk09/improving_functions/improve1.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include<iostream>
+#include<numeric>
 using namespace std;
 /**
  * @brief creates a vector of integers with ascending numbers from 0 - n-1
@@ -9,14 +10,13 @@ using namespace std;
  * @param n the size of the vector to be created
  * @return std::vector<int>
  */
+
+//Tiiä mitä vittua täsä pitäs vielä hinkata
 std::vector<int> ascendingVector(int n){
-    std::vector<int> v;
-    int i = 0;
-    // push_back() amortized constant O(n)
-    while (  i!= n) {     //O(n), muutettu != koska vertailee vain onko sama vai ei
-        v.push_back(i);             // a.constant
-        ++i;
-    }
+    std::vector<int> v(n-1);
+    iota(v.begin(), v.end(), 0);
+
+
 
     return v;
 }
