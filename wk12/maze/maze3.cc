63866c3a50fef7d16eb80245ba05266588b34411
Vesa Lukkarila <vesalukkarila@MacBook-Pro-5.local>
2022-11-25 17:21:33 +0200

    3structinherja

--- wk12/maze/maze3.cc
+++ wk12/maze/maze3.cc
@@ -18,12 +18,31 @@ using namespace std;
 void connectNodes(Node& node1, Node& node2) {
 
     // Student code here to connect the nodes together
-
+    //sen mielestä ei ole connected, pitääkö hajottaa if ja lisätästructin above, below kohtiin ne linkitetyt, osaako se katsoa sitä kun nyt on to ja from
+/*
     if ((node1.x == node2.x || node1.x == node2.x +1 || node1.x == node2.x -1)
             &&
             (node1.y == node2.y || node1.y == node2.y +1 || node1.y == node2.y -1) ){
 
         node1.to = &node2;
         node2.from = &node1;
+    }
+    */
+    if (node1.x == node2.x && node1.y == node2.y -1){
+        node1.above = &node2;
+        node2.below = &node1;
+    }
+
+    if (node1.x == node2.x && node1.y == node2.y +1){
+        node1.below = &node2;
+        node2.above = &node1;
+    }
+    if (node1.x == node2.x-1 && node1.y == node2.y ){
+        node1.right = &node2;
+        node2.left = &node1;
+    }
+    if (node1.x == node2.x +1 && node1.y == node2.y ){
+        node1.left = &node2;
+        node2.right = &node1;
     }
 }
